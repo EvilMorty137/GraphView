45f9c6e4c9ccad0ea83281c1ddaef07af423f4da
peijingdong <peijingdong@angelalign.com>
2018-11-21 12:11:57 +0800

    c1

--- GraphView/MainWindow.xaml.cs
+++ GraphView/MainWindow.xaml.cs
@@ -934,12 +934,9 @@ namespace GraphView
 
         }
         
-        private void testGit()
+        void c1()
         {
-            //dev1 commit1 
-            //dev1 commit2 
-            //dev1 commit3 
-        }
 
+        }
     }
 }
